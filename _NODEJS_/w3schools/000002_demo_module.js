3a85ea5755b1187f4f37df46e7408ce3d6c3dde6
Tuan Tran <mptnt1988@gmail.com>
2017-11-29 04:31:05 -0800

    Add w3schools nodeJS examples

---
+++ _NODEJS_/w3schools/000002_demo_module.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var dt = require('./000002_myfirstmodule');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(8080);
